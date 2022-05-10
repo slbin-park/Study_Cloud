144991eb66711b65289d697e132acc8e74492841
slbin-park <smpts00@naver.com>
2022-05-10 19:16:47 +0900

    Add register funtion

--- src/components/UI/atoms/input/index.tsx
+++ src/components/UI/atoms/input/index.tsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState, useRef } from 'react';
 import InputComponent from './input';
 import {InputType} from './inputType'
 
-const Button: React.FC<InputType> = (props, {}: InputType) => (
+const Button: React.FC<InputType> = (props, {
+    
+}: InputType) => (
 <InputComponent {...props} />
 );
 
