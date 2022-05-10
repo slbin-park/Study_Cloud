23e2d6546041e1b4cf4bd8d3c6f87224b6bed3c8
slbin-park <smpts00@naver.com>
2022-05-10 19:49:19 +0900

    Edit README.md

--- src/components/UI/pages/loginpage/loginpageStyle.tsx
+++ src/components/UI/pages/loginpage/loginpageStyle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { LoginPageType } from './loginpageType';
 
-const LoginPageStyle = styled.div.attrs((props) => ({}))<LoginPageType>`
+const LoginPageStyle = styled.form.attrs((props) => ({}))<LoginPageType>`
 ${(props) => {
     const middle = props.theme.palette.$color_middle;
 return css`
